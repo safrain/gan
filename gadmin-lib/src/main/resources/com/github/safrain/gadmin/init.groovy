8454ca014961ee30129379d74c8663a195b5c6e8
醉风 <zuifeng.zyj@taobao.com>
2013-09-17 19:44:22 +0800

    Add example project

--- gadmin-lib/src/main/resources/com/github/safrain/gadmin/init.groovy
+++ gadmin-lib/src/main/resources/com/github/safrain/gadmin/init.groovy
@@ -1,3 +1,5 @@
+package com.github.safrain.gadmin
+
 import com.github.safrain.gadmin.GAdminFilter
 
 GAdminFilter.loadFromClasspath(GAdminFilter.DEFAULT_RESOURCE_PACKAGE + 'spring.groovy', GAdminFilter.DEFAULT_CHARSET);
--- gadmin-lib/src/main/resources/com/github/safrain/gadmin/spring.groovy
+++ gadmin-lib/src/main/resources/com/github/safrain/gadmin/spring.groovy
@@ -1,3 +1,4 @@
+package com.github.safrain.gadmin
 /**
  * Spring framework support
  */
